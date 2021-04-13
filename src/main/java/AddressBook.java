3e04e4463cb5030e773c3cf540e1d13a83b65e39
Jessica Yu <Jessica3190@msn.com>
2021-04-13 19:09:25 -0400

    renamed addEntry to addContact for clarity

--- src/main/java/AddressBook.java
+++ src/main/java/AddressBook.java
@@ -2,23 +2,30 @@ import java.util.ArrayList;
 import java.util.function.Predicate;
 
 public class AddressBook {
-      ArrayList<AddressEntry> entries = new ArrayList<>();
+      static ArrayList<AddressEntry> entries = new ArrayList<>(); //Arraylist that holds all the entries from AddressEntry
 
 
 public AddressBook(){
 
 }
 
-public void addEntry(AddressEntry entry){
+public void addContact(AddressEntry entry){
     entries.add(entry);
 }
-public void print(Predicate<AddressEntry> predicate) {
+
+public void removeContact(AddressEntry entry){
+    entries.remove(entry);
+}
+
+public static void print(Predicate<AddressEntry> predicate) {
     for (AddressEntry entry : entries){
         if(predicate.test(entry)){
             System.out.println(entry);
-        }
+        }else System.out.println("??");
     }
-
-    //(Sort by name, then surname)
 }
+
+
+
+
 }
