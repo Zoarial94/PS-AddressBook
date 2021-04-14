b5b661643d65666bf194b1bf64515f08edf5a2fa
Jessica Yu <Jessica3190@msn.com>
2021-04-13 22:04:38 -0400

    added functional interfaces for find(), delete(), and edit()

--- src/main/java/AddressBook.java
+++ src/main/java/AddressBook.java
@@ -1,20 +1,43 @@
 import java.util.ArrayList;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class AddressBook extends ArrayList<AddressEntry> {
 
-    public AddressBook(){
+    public AddressBook() {
 
     }
 
     public void print(Predicate<AddressEntry> predicate) {
-        for (AddressEntry entry : this){
-            if(predicate.test(entry)){
+        for (AddressEntry entry : this) {
+            if (predicate.test(entry)) {
                 System.out.println(entry);
             }
         }
-
+    }
+        public void find(Predicate<AddressEntry> predicate) {
+            for (AddressEntry entry : this) {
+                if (predicate.test(entry)) {
+                    System.out.println(entry);
+                }
+            }
 
     }
+    //
+    public void delete(Predicate<AddressEntry> predicate) {
+        for (AddressEntry entry : this) {
+            if (predicate.test(entry)) {
+                System.out.println(entry);
+            }
+        }
+    }
+
+    //Consumer accepts a single input argument and return no result
+    public void edit(String lastName, String firstName, Consumer<AddressEntry> consumer){
+        for (AddressEntry entry : this) {
+                consumer.accept(entry);
+        }
+    }
 }
+
 
